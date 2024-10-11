039cc0421f1a6ae3ca96c35a915c2fd4e213ead4
˜guedesgeorge˜ <george.emannuelcarvalho@gmail.com>
2024-10-11 11:57:06 -0400

    NotaAluno

---
+++ src/BoletimEstadantil.java
@@ -0,0 +1,14 @@
+public class BoletimEstadantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 8;
+
+        if (mediaFinal <6) {
+            System.out.println("Reprovado");
+        } else if (mediaFinal==6) {
+            System.out.println("Voce esta de exame");
+
+        }else
+            System.out.println("APROVADO");
+    }
+}
